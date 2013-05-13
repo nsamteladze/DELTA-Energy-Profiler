73fb1ea2ccb281068fe2b5b1c225c823f071bfd8
Nikolai Samtealdze <nikolay.samtealdze@gmail.com>
2013-05-13 04:52:39 -0400

    Updated the experiments setup
    1. Real data URLs
    2. Proper number of repetitions

--- src/com/samteladze/delta/energy_profiler/ExperimentOptionsFactory.java
+++ src/com/samteladze/delta/energy_profiler/ExperimentOptionsFactory.java
@@ -14,12 +14,26 @@ import com.samteladze.delta.energy_profiler.utils.MyLogger;
 public class ExperimentOptionsFactory {
 	
 	private final static long TIME_BETWEEN_MEASUREMENTS = 30000;
-	private final static int IDLE_NUMBER_OF_MEASUREMENTS = 200;
-	private final static int SCREEN_NUMBER_OF_MEASUREMENTS = 200;
-	private final static int CPU_NUMBER_OF_MEASUREMENTS = 200;
-	private final static int NET_NUMBER_OF_MEASUREMENTS = 50;
+	private final static int IDLE_NUMBER_OF_MEASUREMENTS = 240;
+	private final static int SCREEN_NUMBER_OF_MEASUREMENTS = 180;
+	private final static int CPU_NUMBER_OF_MEASUREMENTS = 180;
+	// For 1 MB file
+	// TEMP Regular = 50
+	private final static int NET_NUMBER_OF_MEASUREMENTS_1 = 5;
+	private final static String NET_DONWLOADED_FILE_URL_1 = "http://www.csee.usf.edu/~nsamteladze/res/test_1.dat";
+	// For 5 MB file
+	@SuppressWarnings("unused")
+	private final static int NET_NUMBER_OF_MEASUREMENTS_5 = 30;
+	@SuppressWarnings("unused")
+	private final static String NET_DONWLOADED_FILE_URL_5 = "http://www.csee.usf.edu/~nsamteladze/res/test_5.dat";
+	// For 10 MB file
+	@SuppressWarnings("unused")
+	private final static int NET_NUMBER_OF_MEASUREMENTS_10 = 20;
+	@SuppressWarnings("unused")
+	private final static String NET_DONWLOADED_FILE_URL_10 = "http://www.csee.usf.edu/~nsamteladze/res/test_10.dat";
+	
 	private final static String CPU_TEST_ARCHIVE = "test.zip";
-	private final static String NET_DONWLOADED_FILE_URL = "http://www.csee.usf.edu/~nsamteladze/res/resume.pdf";
+	
 	
 	public static ExperimentOptions instantiate(ExperimentType experimentType) {
 		
@@ -32,8 +46,8 @@ public class ExperimentOptionsFactory {
 			return new CPUZipExperimentOptions(experimentType, CPU_NUMBER_OF_MEASUREMENTS, TIME_BETWEEN_MEASUREMENTS,  
 					(new File(FileManager.getTestDirAbsolutePath(), CPU_TEST_ARCHIVE)).getAbsolutePath());
 		case Net_4G:
-			return new Net4GExperimentOptions(experimentType, NET_NUMBER_OF_MEASUREMENTS, TIME_BETWEEN_MEASUREMENTS, 
-											  NET_DONWLOADED_FILE_URL);
+			return new Net4GExperimentOptions(experimentType, NET_NUMBER_OF_MEASUREMENTS_1, TIME_BETWEEN_MEASUREMENTS, 
+											  NET_DONWLOADED_FILE_URL_1);
 		default:
 			MyLogger.LogError("Failed to instantiate experiment options; experiment type was not found", 
 							  ExperimentOptionsFactory.class.toString());
